6f8d80ddb0b371088921da398aa125fc7b25c86a
Cletus Kingdom <cletuskingdom@gmail.com>
2022-02-24 19:45:44 +0100

    i can select multiple options in a select button

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/', function (Request $req) {
+    return $req->state;
+    foreach ($req->state as $data) { 
+        $datas = $data;
+    }
+    return $datas;
 });
